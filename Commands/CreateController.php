0e58aafe45b6acf3c8ce2bc0c078bc92abd6a31f
cjam <cjamcu@gmail.com>
2018-08-23 19:17:45 -0400

    now can create controller from ClI

---
+++ Commands/CreateController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cjam
+ * Date: 8/23/2018
+ * Time: 6:52 p.m.
+ */
+
+namespace FastCode\Commands;
+
+use CodeIgniter\CLI\CLI;
+use CodeIgniter\CLI\BaseCommand;
+use FastCode\Libraries\Generate;
+
+class CreateController extends BaseCommand
+{
+    use Generate;
+    protected $group = 'FastCodeCI';
+    protected $name = 'create:controller';
+    protected $description = 'Create a  Controller file';
+    protected $data = [];
+
+
+
+    public function run(array $params)
+    {
+        $name = array_shift($params);
+
+        if (empty($name))
+        {
+            $name = CLI::prompt('Controller Name');
+        }
+
+        $data = [
+            'name'      =>  ucfirst($name),
+            'space' =>  'App',
+        ];
+
+        $content = $this->render('SimpleController',$data);
+        $path = $this->getPathOutput('Controllers').$data['name'].'.php';
+        $this->copyFile($path,$content);
+
+        echo "File created :" . $path;
+
+
+    }
+}
---
+++ Templates/SimpleController.php
@@ -0,0 +1,11 @@
+@php namespace {! space !}\Controllers;
+
+use CodeIgniter\Controller;
+
+class {! name !} extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
